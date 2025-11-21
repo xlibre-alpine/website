178a41f0e943b4af40f18b8c52cabef405a6b139
matik <89382946+matu6968@users.noreply.github.com>
2025-11-22 00:51:12 +0100

    Fix grep command for Alpine Linux detection (Alpine uses unquoted distro ID, while Chimera uses quoted one)

--- main/install.sh
+++ main/install.sh
@@ -13,7 +13,7 @@ wget -O /etc/apk/keys/xlibre.rsa.pub https://xlibre-alpine.github.io/website/mai
 if grep -qi 'ID="chimera"' /etc/os-release; then
     echo "Detected Chimera Linux"
     REPO_URL="https://xlibre-alpine.github.io/website/chimera/main"
-elif grep -qi '^ID="alpine"' /etc/os-release; then
+elif grep -qi '^ID=alpine' /etc/os-release; then
     echo "Detected Alpine Linux"
     REPO_URL="https://xlibre-alpine.github.io/website/main"
 else
